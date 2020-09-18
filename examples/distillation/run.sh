4409c0a321e0d0c7a011eee2bbeb441e5ca2ee63
Ida Novindasari <idanovinda@gmail.com>
2020-09-18 10:04:56 +0000

    modified the parameters for running distillation

--- examples/distillation/run.sh
+++ examples/distillation/run.sh
@@ -1,17 +1,15 @@
-export CUDA_VISIBLE_DEVICES=$1
-
-BASE_DIR="/datasets/huggingface/wikipedia/wikipedia_100"
-FILENAME="wikipedia_100.txt"
-BINARIZED_FILENAME="binarized_wikipedia_100"
+export BASE_DIR=$1
+export FILENAME=$2
+export CUDA_VISIBLE_DEVICES=$3
 
 python scripts/binarized_data.py \
-    --file_path $BASE_DIR/$FILENAME \
+    --file_path $BASE_DIR/$FILENAME.txt \
     --tokenizer_type bert \
     --tokenizer_name bert-base-uncased \
-    --dump_file $BASE_DIR/$BINARIZED_FILENAME
+    --dump_file $BASE_DIR/binarized_$FILENAME
 
 python scripts/token_counts.py \
-    --data_file $BASE_DIR/$BINARIZED_FILENAME.bert-base-uncased.pickle \
+    --data_file $BASE_DIR/binarized_$FILENAME.bert-base-uncased.pickle \
     --token_counts_dump $BASE_DIR/token_counts.bert-base-uncased.pickle \
     --vocab_size 30522
 
@@ -23,6 +21,6 @@ python train.py \
     --alpha_ce 5.0 --alpha_mlm 2.0 --alpha_cos 1.0 --alpha_clm 0.0 --mlm \
     --freeze_pos_embs \
     --dump_path /logs/distilbert/wikipedia_100 \
-    --data_file $BASE_DIR/$BINARIZED_FILENAME.bert-base-uncased.pickle \
+    --data_file $BASE_DIR/binarized_$FILENAME.bert-base-uncased.pickle \
     --token_counts $BASE_DIR/token_counts.bert-base-uncased.pickle \
     --force
