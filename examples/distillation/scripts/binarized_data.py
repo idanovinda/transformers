4a9267b98a0802e64191b26144955710b53c177e
Ida Novindasari <idanovinda@gmail.com>
2020-09-21 07:46:56 +0000

    add max 512 words

--- examples/distillation/scripts/binarized_data.py
+++ examples/distillation/scripts/binarized_data.py
@@ -69,7 +69,7 @@ def main():
     start = time.time()
     for text in data:
         text = f"{bos} {text.strip()} {sep}"
-        token_ids = tokenizer.encode(text, add_special_tokens=False)
+        token_ids = tokenizer.encode(text, add_special_tokens=False)[:512]
         rslt.append(token_ids)
 
         iter += 1
