1f38e769f50bcc6c935f1d405de820180f1c28dc
Ida Novindasari <idanovinda@gmail.com>
2020-09-22 17:05:31 +0200

    only run binarized

--- examples/distillation/run.sh
+++ examples/distillation/run.sh
@@ -10,20 +10,20 @@ python scripts/binarized_data.py \
     --cache_dir $CACHE_DIR \
     --dump_file $BASE_DIR/binarized_$FILENAME
 
-python scripts/token_counts.py \
-    --data_file $BASE_DIR/binarized_$FILENAME.bert-base-uncased.pickle \
-    --token_counts_dump $BASE_DIR/token_counts.bert-base-uncased.pickle \
-    --vocab_size 30522
+# python scripts/token_counts.py \
+#     --data_file $BASE_DIR/binarized_$FILENAME.bert-base-uncased.pickle \
+#     --token_counts_dump $BASE_DIR/token_counts.bert-base-uncased.pickle \
+#     --vocab_size 30522
 
-python train.py \
-    --student_type distilbert \
-    --student_config training_configs/distilbert-base-uncased.json \
-    --teacher_type bert \
-    --teacher_name bert-base-uncased \
-    --cache_dir $CACHE_DIR \
-    --alpha_ce 5.0 --alpha_mlm 2.0 --alpha_cos 1.0 --alpha_clm 0.0 --mlm \
-    --freeze_pos_embs \
-    --dump_path /logs/distilbert/$FILENAME \
-    --data_file $BASE_DIR/binarized_$FILENAME.bert-base-uncased.pickle \
-    --token_counts $BASE_DIR/token_counts.bert-base-uncased.pickle \
-    --force
+# python train.py \
+#     --student_type distilbert \
+#     --student_config training_configs/distilbert-base-uncased.json \
+#     --teacher_type bert \
+#     --teacher_name bert-base-uncased \
+#     --cache_dir $CACHE_DIR \
+#     --alpha_ce 5.0 --alpha_mlm 2.0 --alpha_cos 1.0 --alpha_clm 0.0 --mlm \
+#     --freeze_pos_embs \
+#     --dump_path /logs/distilbert/$FILENAME \
+#     --data_file $BASE_DIR/binarized_$FILENAME.bert-base-uncased.pickle \
+#     --token_counts $BASE_DIR/token_counts.bert-base-uncased.pickle \
+#     --force
