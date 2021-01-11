5281c1aa7368e14c4b5932d104359c5899c59eb2
Ida Novindasari <idanovinda@gmail.com>
2021-01-11 17:13:10 +0100

    add training choice

--- examples/distillation/multiple_run.sh
+++ examples/distillation/multiple_run.sh
@@ -1,7 +1,7 @@
-bash training_scripts/init-bbu-0247911_label-rms-1M-1-original_ce-loss-1_roberta.sh /datasets/wikipedia_roberta wikipedia_0 0,1,2,3,4,5,6,7 8 1 1
+bash training_scripts/init-bbu-0247911_label-rms-1M-1-original_ce-loss-1_roberta.sh /datasets/wikipedia_1000 wikipedia_1000 0,1,2,3,4,5,6,7 8 1 1 0
 
-bash training_scripts/init-bbu-0247911_label-rb-10M-1-original_ce-loss-1_roberta.sh /datasets/wikipedia_roberta wikipedia_0 0,1,2,3,4,5,6,7 8 1 1
+bash training_scripts/init-bbu-0247911_label-rb-10M-1-original_ce-loss-1_roberta.sh /datasets/wikipedia_1000 wikipedia_1000 0,1,2,3,4,5,6,7 8 1 1 0 
 
-bash training_scripts/init-bbu-0247911_label-rb-100M-1-original_ce-loss-1_roberta.sh /datasets/wikipedia_roberta wikipedia_0 0,1,2,3,4,5,6,7 8 1 1
+bash training_scripts/init-bbu-0247911_label-rb-100M-1-original_ce-loss-1_roberta.sh /datasets/wikipedia_1000 wikipedia_1000 0,1,2,3,4,5,6,7 8 1 1 0
 
-bash training_scripts/init-bbu-0247911_label-rb-1B-1-original_ce-loss-1_roberta.sh /datasets/wikipedia_roberta wikipedia_0 0,1,2,3,4,5,6,7 8 1 1
+bash training_scripts/init-bbu-0247911_label-rb-1B-1-original_ce-loss-1_roberta.sh /datasets/wikipedia_1000 wikipedia_1000 0,1,2,3,4,5,6,7 8 1 1 0
